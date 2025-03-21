6c9de5972a9422fd1fb14677920a7fa0f6b01688
Abhivyakti <palabhivyakti@gmail.com>
2025-03-21 14:32:36 +0530

    -removed old files

--- backend/routes/search.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { searchByImage, searchByText } from "../controllers/search.controller.js";
-
-const router = express.Router();
-
-router.post("/image", searchByImage);
-router.post("/text", searchByText);
-
-export default router;
--- frontend/src/components/common/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = ({ size = "md", theme = "light" }) => {
-	const sizeClass = `loading-${size}`;
-	const colorClass = theme === "dark" ? "text-light-green" : "text-teal";
-  
-	return <span className={`loading loading-spinner ${sizeClass} ${colorClass}`} />;
-  };
-  
-  export default LoadingSpinner;
-  
