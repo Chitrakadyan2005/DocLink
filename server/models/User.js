6ce7c86ea3bbaf29d5aae863a5f7701d6e8f0810
Chitrakadyan2005 <143112769+Chitrakadyan2005@users.noreply.github.com>
2025-03-21 16:24:05 +0530

    Add files via upload

---
+++ server/models/User.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  specialty: {
+    type: String,
+    required: true
+  },
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  profileImage: {
+    type: String,
+    default: ''
+  },
+  coverImage: {
+    type: String,
+    default: ''
+  },
+  location: String,
+  hospital: String,
+  bio: String,
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
